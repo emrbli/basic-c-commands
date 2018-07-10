bc8adef10877aefb4550b505dd198244a587493d
Kemal Emre Ballı <33103502+kemaleb@users.noreply.github.com>
2018-07-10 17:34:02 +0300

    C dilinde 1'den 10'a kadar olan sayıların yazımı

---
+++ 1_den_10_a_kadar_sayılar.c
@@ -0,0 +1,20 @@
+# C
+Software Examples
+#include<stdio.h>
+#include<conio.h>
+
+
+
+main() {//1 den 10 a kadar olan sayıların toplamı
+
+	int toplam=0 ;
+	for(int i=1; i<11; i++) 
+	toplam +=i; //toplam = toplam+1
+printf("Toplam = %d",toplam);
+getch();	
+	
+	
+		
+	
+}
+
